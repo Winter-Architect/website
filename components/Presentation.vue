72f54f48548b46aac971a84accbb952420b4268c
Nocteln <eliottmieze@gmail.com>
2024-11-01 21:51:42 +0100

    debut du mode histoire

---
+++ components/Presentation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="bg-pearl-bush-100 w-[80vw] p-5 pt-10 relative m-10 mb-20 rounded-md"
+  >
+    <h1
+      class="absolute -top-10 -left-10 bg-pearl-bush-100 border-outer-space-950 border-4 p-5 rounded-full h-[10vh] w-[20vw] flex justify-start items-center"
+    >
+      <div class="flex items-end gap-1">
+        <span class="text-3xl font-bold leading-none">{{ number }}.</span>
+        <span class="text-2xl leading-none">{{ name }}</span>
+      </div>
+    </h1>
+
+    <slot />
+  </div>
+</template>
+
+<script setup>
+const { name, number } = defineProps(["name", "number"]);
+</script>
--- pages/story.vue
+++ pages/story.vue
@@ -1,3 +1,83 @@
 <template>
-  <h1>story</h1>
+  <div>
+    <Presentation number="1" name="Notre Origine">
+      <p class="indent-8">
+        En tant que groupe de jeunes étudiants, nous avons décidé de nous ouvrir
+        au monde de l'entreprise en fondant notre propre start-up, afin de
+        travailler sur des projets qui nous tiennent chacun à cœur ainsi que
+        pour offrir des contenus qui ravissent aussi bien nos membres que les
+        clients.<br />
+      </p>
+      <p class="indent-8">
+        C'est donc un honneur pour nous de vous présenter
+        <strong>Winter Architect</strong>, start-up que nous souhaitons orienter
+        vers le développement de jeux vidéo. Ce nom n'est pas anodin puisqu'il
+        s'agit de la base de notre évolution, étant lié à nos tout premier
+        projet de jeu Blackout.<br />
+      </p>
+      <p class="indent-8">
+        En effet, dans le lore de notre jeu, Winter Architect est la faction la
+        plus importante, l'exploration de leurs (nos) locaux permet au joueur de
+        se rendre compte de l'étendue des secrets et des anomalies qui y sont
+        enfermés ainsi que de l'étendue des consequences qu'aurait cette
+        entreprise sur l'humanité si la moindre de leurs innovations venaient à
+        s'enfuir ou à être capturé.
+      </p>
+    </Presentation>
+    <Presentation number="2" name="Nos Valeurs">
+      <p class="indent-8">
+        Nous accordons une importance considérable à ce qui se répercute sur le
+        sentiment du joueur. En effet, plus qu'un marionnettiste qui se sert de
+        sa marionnette, nous voulons qu'il s'identifie à celle-ci ou qu'à minima
+        il ressente ce que le personnage puisse ressentir. Nous aspirons à
+        procurer un sentiment gratifiant d'accomplissement en offrant la
+        possibilité de finir le jeu, de le compléter. Ce sentiment est
+        primordial dès lors qu'il donne la sensation au joueur d'avoir avancé
+        dans son périple, d'avoir joué pour atteindre un objectif, mais
+        principalement d'avoir vécu une histoire. Pour faire simple, nous
+        voulons que nos joueurs vivent l'histoire du personnage sans être
+        parcourus par un sentiment d'inachevé ou encore de redondance.<br />
+      </p>
+      <p class="indent-8">
+        C'est ainsi que nous proposons une expérience unique pour chacun de nos
+        jeux, dont l'obtention se fait par un unique achat. En effet, notre
+        dessein est d'offrir une expérience immédiate et immersive au joueur
+        alors qu'il se décide à lancer le jeu, en nous écartant de l'aspect
+        sangsue du modèle économique courant et commun dans les jeux
+        indépendants, tels que des achats en jeu pour des cosmétiques ou encore
+        des publicités.
+        <span class="italic text-sm">Un achat, une expérience.</span><br />
+      </p>
+    </Presentation>
+    <Presentation number="3" name="Notre jeu">
+      <p class="indent-8">
+        Blackout est un 3D platformer / puzzle solver qui pousse les joueurs à
+        la réflexion dans une ambiance d'horreur. Un élement central de
+        l'éxpérience Blackout est son aspect duo coopératif : <br />
+        Deux joueurs se retrouvent dans un complexe de salles generées
+        pseudo-aléatoirement (Voir génération des salles) avec un affichage à
+        l'écran quasi-identique commun mais des contrôles différents :
+      </p>
+      <ul class="list-disc list-inside ml-4 space-y-2">
+        <li>
+          Le premier joueur, surnommé <strong>Agent</strong> utilise des
+          contrôles "classiques" : mouvement bidimensionnel, saut, interaction
+          avec PNJs et autres objets interactables, recupération d'objets... et
+          aura pour mission d'explorer le terrain, comprendre l'épreuve offerte
+          par la salle et survivre aux attaques ennemies.
+        </li>
+        <li>
+          Le second joueur; surnommé <strong>Support</strong> possède des
+          contrôles plus uniques : Il peut intéragir avec un sous-ensemble des
+          objets du jeu appelés les <span class="italic">éléctroniques</span>,
+          il s'agit entre-autres de bras robotiques, portes, aimants, véhicules,
+          caméras etc... Le support est ainsi indispensable à la résolution des
+          épreuves, il déplace les caméras pour obtenir de meilleurs angles de
+          vue, transporte les objets pour permettre l'avancement du joueur et
+          plein d'autres possibilités faisant appel à la créativité et à la
+          dextérité du joueur.
+        </li>
+      </ul>
+    </Presentation>
+  </div>
 </template>
